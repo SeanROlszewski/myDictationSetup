5a3c5aae655e38de888925a4fc774bce5aa265bb
SeanROlszewski <sean.r.olszewski@gmail.com>
2017-07-15 16:29:43 -0400

    Replacec cp with ditto in install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 cp ./Archive/com.apple.speech.recognition.AppleSpeechRecognition.CustomCommands.plist ~/Library/Preferences/
 echo "installed custom dictation commands"
-cp -r ./Alfred\ 2.app/ /Volumes//Macintosh\ HD/Applications/
+ditto ./Alfred\ 2.app/ /Volumes/Macintosh\ HD/Applications/Alfred\ 2.app
 echo "installed Alfred 2"
-cp -r ./Keyboard\ Maestro.app/ /Volumes/Macintosh\ HD/Applications/
+ditto ./Keyboard\ Maestro.app/ /Volumes/Macintosh\ HD/Applications/Keyboard\ Maestro.app
 echo "installed Keyboard Maestro"
 open ./Archive/Keyboard\ Maestro/String\ Operations\ and\ Other\ Macros.kmmacros
 echo "added Keyboard Maestro macros"
