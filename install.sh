08b62e126aa0669b234695af92fc2b61a1d53af1
SeanROlszewski <sean.r.olszewski@gmail.com>
2017-07-15 16:06:54 -0400

    Update installer script, add Keyboard Maestro macros

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 cp ./Archive/com.apple.speech.recognition.AppleSpeechRecognition.CustomCommands.plist ~/Library/Preferences/
+echo "installed custom dictation commands"
 cp -r ./Alfred\ 2.app/ /Volumes//Macintosh\ HD/Applications/
+echo "installed Alfred 2"
 cp -r ./Keyboard\ Maestro.app/ /Volumes/Macintosh\ HD/Applications/
+echo "installed Keyboard Maestro"
+open ./Archive/Keyboard\ Maestro/String\ Operations\ and\ Other\ Macros.kmmacros
+echo "added Keyboard Maestro macros"
 
 echo "install script completed"
